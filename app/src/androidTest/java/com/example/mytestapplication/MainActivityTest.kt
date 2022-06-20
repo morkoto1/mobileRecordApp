dfcfd9ef282791869b6f499f0d22bafd8ad76a18
morkoto <morkovskytomas@gmail.com>
2022-06-20 12:36:25 +0200

    clean up + add slack notification

---
+++ app/src/androidTest/java/com/example/mytestapplication/MainActivityTest.kt
@@ -0,0 +1,202 @@
+package com.example.mytestapplication
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val overflowMenuButton = onView(
+            allOf(
+                withContentDescription("More options"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.toolbar),
+                        2
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        overflowMenuButton.perform(click())
+
+        Thread.sleep(1500);
+
+        val materialTextView = onView(
+            allOf(
+                withId(androidx.appcompat.R.id.title), withText("Settings"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(androidx.appcompat.R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        materialTextView.perform(click())
+
+        Thread.sleep(1500);
+
+        val floatingActionButton = onView(
+            allOf(
+                withId(R.id.fab),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.app_bar_main),
+                        childAtPosition(
+                            withId(R.id.drawer_layout),
+                            0
+                        )
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton.perform(click())
+
+        Thread.sleep(1500);
+
+        val appCompatImageButton = onView(
+            allOf(
+                withContentDescription("Open navigation drawer"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.toolbar),
+                        childAtPosition(
+                            withClassName(`is`("com.google.android.material.appbar.AppBarLayout")),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageButton.perform(click())
+
+        Thread.sleep(1500);
+
+        val navigationMenuItemView = onView(
+            allOf(
+                withId(R.id.nav_gallery),
+                childAtPosition(
+                    allOf(
+                        childAtPosition(
+                            withId(R.id.nav_view),
+                            0
+                        )
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        navigationMenuItemView.perform(click())
+
+        Thread.sleep(1500);
+
+        val appCompatImageButton2 = onView(
+            allOf(
+                withContentDescription("Open navigation drawer"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.toolbar),
+                        childAtPosition(
+                            withClassName(`is`("com.google.android.material.appbar.AppBarLayout")),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageButton2.perform(click())
+
+        Thread.sleep(1500);
+
+        val navigationMenuItemView2 = onView(
+            allOf(
+                withId(R.id.nav_slideshow),
+                childAtPosition(
+                    allOf(
+                        childAtPosition(
+                            withId(R.id.nav_view),
+                            0
+                        )
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        navigationMenuItemView2.perform(click())
+
+        Thread.sleep(1500);
+
+        val appCompatImageButton3 = onView(
+            allOf(
+                withContentDescription("Open navigation drawer"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.toolbar),
+                        childAtPosition(
+                            withClassName(`is`("com.google.android.material.appbar.AppBarLayout")),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageButton3.perform(click())
+
+        Thread.sleep(1500);
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
